da8dc9d142c22fea4bbca2fb00955a8239ca73e4
AlannaFM <alannamachado84@gmail.com>
2025-09-03 16:26:34 -0300

    refactor: ajustes nas classes para respeitar o princípio LSP

--- src/main/java/br/com/mariojp/solid/lspaccounts/BankService.java
+++ src/main/java/br/com/mariojp/solid/lspaccounts/BankService.java
@@ -1,7 +1,11 @@
 package br.com.mariojp.solid.lspaccounts;
 
 public class BankService {
-    public void processWithdrawal(Account acc, double amount){
-        acc.withdraw(amount);
-    }
+	
+	public void processWithdrawal(Account acc, double amount) {
+		if (acc instanceof Withdrawable saque) {
+			saque.withdraw(amount);
+		}
+	}
+
 }
